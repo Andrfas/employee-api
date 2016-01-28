16ebd5a44723b68178194cdd57ff8d3d39bea16b
Andrey Mikhatsky <andrfas@gmail.com>
2016-01-28 16:22:21 +0200

    some changes

---
+++ api/controllers/CompanyController.js
@@ -0,0 +1,18 @@
+module.exports = {
+    createCompany: function(req, res) {
+        var reqFieldsPresent = CommonFunctions.areKeysInObj(reqFields.createCompany, req.body);
+        if(reqFieldsPresent !== true) {
+            return res.json({error:'Missed requeired field: '+reqFieldsPresent})
+        }
+
+        return res.ok();
+    }
+}
+
+// felds, that are requeired for request. Should be for each function, and should have the same name
+var reqFields = {
+    createCompany: [
+        'name',
+        'description'
+    ]
+}
--- assets/js/controllers/IndexCntrl.js
+++ assets/js/controllers/IndexCntrl.js
@@ -9,4 +9,8 @@ app.controller('IndexCntrl', ['$scope', function($scope) {
     $scope.selectCity = function() {
         console.log('city', $scope.selectedCity);
     }
+
+    console.log(3, areKeysInObj(['a', 'true'], {a:'asd', 'true':1}) === true);
+
+    
 }])
