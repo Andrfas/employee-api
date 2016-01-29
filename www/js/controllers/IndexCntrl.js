164f20f85254b45e47562af9b8538a67d2905cc6
Andrey Mikhatsky <andrfas@gmail.com>
2016-01-29 17:10:31 +0200

    builded sails app

---
+++ www/js/controllers/IndexCntrl.js
@@ -0,0 +1,13 @@
+app.controller('IndexCntrl', ['$scope', function($scope) {
+    $scope.selectedCity;
+
+    $scope.getCities = function(str) {
+        // console.log('searchStr', str);
+        return ['Kyiv', 'Moscow'];
+    }
+
+    $scope.selectCity = function() {
+        console.log('city', $scope.selectedCity);
+    }
+    
+}])
