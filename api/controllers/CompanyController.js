8fa43905dfd1f53e3b1571e59f6e90d3fdac0d88
berezynskyi <makc.bereza@gmail.com>
2016-01-30 06:53:13 +0200

    crypt passwords, create company is fully done

--- api/controllers/CompanyController.js
+++ api/controllers/CompanyController.js
@@ -1,6 +1,7 @@
 var requestsDB = require('../../api/services/requestsDB.js')
 var CommonFunctions = require('../../api/services/CommonFunctions.js')
 var _ = require('lodash')
+var async = require('async')
 
 module.exports = {
     createCompany: createCompany
@@ -17,26 +18,43 @@ function createCompany(req, res) {
     delete company.email 
     delete company.password
      
-    requestsDB.create('Company', req.body, function(response){
-        if (response.status) {
-            return res.json({status:response.status, msg:'[CompanyController createCompany company] '+response.msg})
-        }
+    async.waterfall([
+        function(callback) {
+            requestsDB.create('Company', req.body, function(response){
+                if (response.status) {
+                    return res.json({status:response.status, msg:'[CompanyController createCompany company] '+response.msg})
+                }
+            callback(null, response._id);
+            })
+        },
+        function(id, callback) {
 
-        var credentials = {
-            company_id: response._id,
-            email: req.body.email,
-            password: req.body.password
-        }
 
-        requestsDB.create('Credentials', credentials, function(response){
-            if (response.status) {
-                return res.json({status:response.status, msg:'[CompanyController createCompany credentials] '+response.msg})
+            var credentials = {
+                company_id: id,
+                email: req.body.email,
+                password: req.body.password
             }
+            requestsDB.changePass(credentials, function(response){
+                if (response.status) {
+                    return res.json({status:response.status, msg:'[CompanyController createCompany credentials] '+response.msg})
+                }
+                callback(response)
+            })
 
-            return res.ok();
-        })
-            
-    })
+            // var company = new db(credentials)
+            // company.changePass(req.body.password)
+            // company.save()
+            // requestsDB.create('Credentials', credentials, function(response){
+            //     if (response.status) {
+            //         return res.json({status:response.status, msg:'[CompanyController createCompany credentials] '+response.msg})
+            //     }
+            // })
+        }
+    ], function (result) {
+        return res.ok({status: 200});
+    });
+
 }
 
 // felds, that are requeired for request. Should be for each function, and should have the same name
--- api/services/requestsDB.js
+++ api/services/requestsDB.js
@@ -5,11 +5,22 @@ module.exports = {
     create: function(model, obj, callback) {
     	db[model].create(obj, function(err, res){
     		if (err) {
-    			callback({status: 400, msg:'[requestsDB service] create error'})
+    			callback({status: 400, msg:'[requestsDB service] create error: '+err.errmsg})
     			return;
     		}
 
     		callback(res)
     	})
+    },
+    changePass: function(obj, callback){
+        var dbModel = new db.Credentials(obj)
+        dbModel.changePass(obj.password)
+        dbModel.save(function(err, res){
+        	if (err) {
+    			callback({status: 400, msg:'[requestsDB service] create error: '+err.errmsg})
+    			return;
+    		}
+        	callback(res)
+        })
     }
 }
