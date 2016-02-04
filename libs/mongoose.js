268264ba389540c62d706970bdad9c9dcdaeac73
Andrey Mikhatsky <andrfas@gmail.com>
2016-02-04 11:19:00 +0200

    fixed /libs/mongoose.js console output

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -3,42 +3,19 @@ var mongoose = new Mongoose();
 var dbConf = require('../config/connections.js').connections;
 var dbData = dbConf[dbConf.mode];
 
- 
-
-/**
- * We check if the connection is ok
- * If so we will continue to load everything ...
- */
-// var db = mongoose.connection;
-
-
-// db.on('error', function() {
-//     console.log('Mongoose connection error');
-// });
-// db.once('open', function callback() {
- 
-//     console.log('Connected to MongoDB !');
- 
-// });
 
 var models = require('../api/models/models.js');
-var mong = {};
+var mongooseModels = {};
 for(key in models) {
-    mong[key] = models[key](mongoose);
+    mongooseModels[key] = models[key](mongoose);
 }
 
 mongoose.connect(dbData.host+dbData.dbName, function(err) {
-    console.error(2, err)
+    if(err) {
+        console.log('Error: ', err);
+    } else {
+        console.log('Connected to MongoDB !');
+    }
 });
  
-module.exports = mong;
-
-/**
- * Mongodb Schemas/Models
- */
- // module.exports = {
- 
- //     Company: require('../api/models/Company.js')(mongoose),
- //     Credentials: require('../api/models/Credentials.js')(mongoose),
- 
- // }
+module.exports = mongooseModels;
