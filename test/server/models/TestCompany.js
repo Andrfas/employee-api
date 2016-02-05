3daddabfea3c4ca5c5e80ce3e2558f2a151a8784
Andrey Mikhatsky <andrfas@gmail.com>
2016-02-05 02:12:55 +0200

    refactored testing company model

---
+++ test/server/models/TestCompany.js
@@ -0,0 +1,26 @@
+var sinon = require('sinon');
+var assert = require('assert');
+var requestsDB = require('../../../api/services/requestsDB.js');
+
+describe("api/CompanyModel", function(){ 
+    describe('Successful scenario.', function(){
+        it("Should create company with name and description directly in db", function(done){   
+                requestsDB.create('Company', testData.company, function(err, res){
+                    // console.log(res)
+                    assert.strictEqual(err, null);
+                    assert.equal(res.name, testData.company.name); 
+                    assert.equal(res.description, testData.company.description); 
+                    done();
+                    })  
+        });
+    })
+})
+
+var testData = {
+    company: {
+        name: "MaxCorp",
+        description: "The best company in the world",
+        email: "andrfas@gmail.com",
+        password: "testtest"    
+    }
+}
