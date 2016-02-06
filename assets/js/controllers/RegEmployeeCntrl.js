3346f8316a07eebc504ab7657724ba0924b524f6
Andrey Mikhatsky <andrfas@gmail.com>
2016-02-06 23:35:35 +0200

    added assets to ignore

--- assets/js/controllers/RegEmployeeCntrl.js
+++
@@ -1,7 +0,0 @@
-app.controller('RegEmployeeCntrl', ['$scope', function($scope) {
-    $scope.name = '';
-    $scope.stud = 'stud1';
-    $scope.changeEmplSelect = function(person){
-        $scope.stud = person;
-    }
-}])
--- assets/js/controllers/RegisterMainCntrl.js
+++
@@ -1,8 +0,0 @@
-app.controller('RegisterMainCntrl', ['$scope', function($scope) {
-	$scope.choose = ['employee', 'company'];
-    $scope.clientType;
-  	$scope.changeClientSelect = function(person){
-        // console.log(person);
-  		$scope.clientType = person;
-  	}
-}])
