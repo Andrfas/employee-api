50a352992b613afca8a2f98eb7bd9419ebf6dc56
Andrey Mikhatsky <andrfas@gmail.com>
2016-02-07 09:37:00 +0200

    removed extra task

--- tasks/register/concurrent.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (grunt) {
-    grunt.config.set('concurrent', {
-            liftAndWatch: ['liftSails', 'watch']
-        
-    });
-
-    grunt.loadNpmTasks('grunt-concurrent');
-};
-
