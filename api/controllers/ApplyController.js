2263ac3b915299ca6c146f33b0a800d7cb3ce37a
gusa4okgrr <gusa4ok.1996@gmail.com>
2016-05-15 12:16:39 +0300

    Forgotten Function

--- api/controllers/ApplyController.js
+++ api/controllers/ApplyController.js
@@ -13,13 +13,24 @@ module.exports = {
 function createApply (req, res) {
     if (!req.param('employeeId') || !req.param('letter') || !req.param('advertId'))
         return res.badRequest({message: 'employeeId, letter, advertId param is undefined'});
-    requestsDB.create('Messages', {employee_id: req.param('employeeId'), 
-        proposal_id: req.param('advertId'), letter: req.param('letter'),
-        advertTitle: req.param('advertTitle'), companyName: req.param('companyName')}, function(err, response) {
-        if (err) {
-            return res.badRequest(err);
-        }
-        res.json({status:200});
+    async.waterfall([
+    	function (callback) {
+    		requestsDB.findOne('Messages', {employee_id: req.param('employeeId')}, function(err, found){
+    			if (err){
+    				callback(err);
+    			}
+    			callback(null, found);
+    		})
+    	}],
+    function(err, result){
+    	if (result !== null) return res.json({message: "You have already applied for this job"});
+    	if (err) return res.badRequest(err);
+    	requestsDB.create('Messages', {employee_id: req.param('employeeId'), proposal_id: req.param('advertId'), letter: req.param('letter')}, function(err, response) {
+		    if (err) {
+		        return res.badRequest(err);
+		    }
+		    res.json({status:200});
+		})	
     })
 }
 
